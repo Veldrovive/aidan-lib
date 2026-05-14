4772b9958ec8772dd7f91da1c22ddd692379f09e
Aidan Dempster <aidan.dempster@gmail.com>
2026-05-14 12:35:49 -0400

    Changing dino subtree path

--- src/aidan_lib/definitions.py
+++ src/aidan_lib/definitions.py
@@ -11,3 +11,5 @@ if data_dir_str is None:
     data_dir_str = str(ROOT_DIR.parent.parent / "data")
     print("WARNING: DATA_DIR not found in .env file, defaulting to", data_dir_str)
 DATA_DIR = Path(data_dir_str)
+
+DINOV3_DIR = ROOT_DIR / "dinov3_repo"
