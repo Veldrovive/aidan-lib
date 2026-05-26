590b2137d87c75f2752a6a0f02741fd2ae875ae6
Aidan Dempster <aidan.dempster@gmail.com>
2026-05-25 22:26:56 -0400

    Expanded frame rate to other useful quantities

--- src/aidan_lib/video_utils/video_data.py
+++ src/aidan_lib/video_utils/video_data.py
@@ -5,15 +5,24 @@ import cv2
 @dataclass
 class GenericVideoData:
     fps: float
+    width: int
+    height: int
+    frame_count: int
 
 def get_video_data(vid_path: Path) -> GenericVideoData:
     video = cv2.VideoCapture(str(vid_path))
 
     try:
         fps = video.get(cv2.CAP_PROP_FPS)
+        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
     finally:
         video.release()
 
     return GenericVideoData(
-        fps=fps
+        fps=fps,
+        width=width,
+        height=height,
+        frame_count=frame_count
     )
